9cea34c7d84bc0c1615ac5dbdc3d5127e43643b0
Himanshuu03 <himanshujangra2403@gmail.com>
2024-03-10 11:36:39 +0530

    api

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const dbConnect = require("./DataBase/database");
-const mainRouter = require("./Routes/Routes");
+const dbConnect = require("../DataBase/database");
+const mainRouter = require("../Routes/Routes");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 4000;
