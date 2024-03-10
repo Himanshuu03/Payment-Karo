dac5b1496da84dbacc23929d860a3275185182c9
Himanshuu03 <himanshujangra2403@gmail.com>
2024-03-10 11:59:10 +0530

    cors

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const dbConnect = require("../DataBase/database");
 const mainRouter = require("../Routes/Routes");
-const cors = require("cors");
+// const cors = require("cors");
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
 // Using Global Middleware
 app.use(express.json());
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
-    next(); 
-})
+app.use((req, res, next) => {
+    res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+    });
+    next();
+});
 
 // CORS Configuration
-app.use(cors({
-  origin: 'https://payment-karo-c2g4.vercel.app',
-  methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: 'https://payment-karo-c2g4.vercel.app',
+//   methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+//   credentials: true,
+// }));
 
 // Logging middleware for testing purposes
 app.use((req, res, next) => {
