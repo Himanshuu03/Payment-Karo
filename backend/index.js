7fed4d714d0b99b26248214033e32a15f4c15949
Himanshuu03 <himanshujangra2403@gmail.com>
2024-03-10 10:01:23 +0530

    payment-karo-backend

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,13 @@ const app = express();
 // Using Global Middleware
 app.use(express.json());
 app.use(cors({ origin: true }));
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    next();
+  });
 
 app.get("/", (req, res) => {
     res.send("Hello server is running");
