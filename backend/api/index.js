b0d9a46a2b647ae559a7a2bc56e8489d226feaae
Himanshuu03 <himanshujangra2403@gmail.com>
2024-03-10 12:04:48 +0530

    headers-update-2

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,28 +1,16 @@
 const express = require("express");
 const dbConnect = require("../DataBase/database");
 const mainRouter = require("../Routes/Routes");
-// const cors = require("cors");
+var cors = require('cors')
+
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
 // Using Global Middleware
 app.use(express.json());
-app.use((req, res, next) => {
-    res.set({
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "*",
-        "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
-    });
-    next();
-});
+app.use(cors());
 
-// CORS Configuration
-// app.use(cors({
-//   origin: 'https://payment-karo-c2g4.vercel.app',
-//   methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-//   credentials: true,
-// }));
 
 // Logging middleware for testing purposes
 app.use((req, res, next) => {
